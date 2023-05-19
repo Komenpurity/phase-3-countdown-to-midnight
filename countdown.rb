9f66024db6d7d40889c310dbd19b1c28a71e93b7
Komenpurity <jepkorirpurity8@gmail.com>
2023-05-19 10:02:06 +0300

    completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
-end
+    def countdown(num)
+        until (num < 1) 
+            puts "#{num} SECOND(S)!"    
+            num -= 1
+        end
+
+        return "HAPPY NEW YEAR!"
+    end
+
+    def countdown_with_sleep(number)
+        until (number < 1)
+            puts "#{number} SECOND(S)!"
+            number -=1
+            sleep 5
+        end
+        return "HAPPY NEW YEAR!" 
+    end
+    
